3f78bd6d0a10f789cb342167b6078e81cdae77a6
Filipe Brochier <filipe.brochier@gmail.com>
2025-12-21 20:03:11 -0300

    feat: implemented [POST] /password/reset route

---
+++ apps/api/src/http/routes/auth/request-password-recover.ts
@@ -0,0 +1,48 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function requestPasswordRecover(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/recover',
+    {
+      schema: {
+        tags: ['Auth'],
+        summary: 'Request password recovery',
+        body: z.object({
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { email } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (!userFromEmail) {
+        // We don't disclose whether the email exists or not
+        return reply.status(201).send()
+      }
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSWORD_RECOVER',
+          userId: userFromEmail.id,
+        },
+      })
+
+      // Send e-mail with recovery instructions (omitted)
+
+      console.log(`Password recovery code for ${email}: ${code}`)
+
+      return reply.status(201).send()
+    },
+  )
+}
