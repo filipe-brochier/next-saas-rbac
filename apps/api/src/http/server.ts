dac0f8637554ea0d7a43c2947f5b388ee31f68fd
Filipe Brochier <filipe.brochier@gmail.com>
2025-12-22 11:53:08 -0300

    feat: implemented [POST] /sessions/github

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config'
+
 import fastifyCors from '@fastify/cors'
 import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
@@ -11,6 +13,7 @@ import {
 } from 'fastify-type-provider-zod'
 
 import { errorHandler } from './error-handler'
+import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
@@ -41,17 +44,18 @@ app.register(fastifySwaggerUI, {
 })
 
 app.register(fastifyJwt, {
-  secret: 'supersecret',
+  secret: process.env.JWT_SECRET as string,
 })
 
 app.register(fastifyCors)
 
 app.register(createAccount)
 app.register(authenticateWithPassword)
+app.register(authenticateWithGithub)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
 
-app.listen({ port: 3333 }).then(() => {
-  console.log('Server is running on http://localhost:3333')
+app.listen({ port: Number(process.env.APP_PORT) }).then(() => {
+  console.log(`Server is running on http://localhost:${process.env.APP_PORT}`)
 })
