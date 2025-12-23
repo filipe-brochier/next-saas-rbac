8bb898b03c47844974ba7739a615fe9162a58b5a
Filipe Brochier <filipe.brochier@gmail.com>
2025-12-23 19:57:44 -0300

    feat: implemented [DELETE] /organizations/:slug/projects/:projectId

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -26,6 +26,7 @@ import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { createProject } from './routes/projects/create-project'
+import { deleteProject } from './routes/projects/delete-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -81,6 +82,7 @@ app.register(shutdownOrganization)
 app.register(transferOrganization)
 
 app.register(createProject)
+app.register(deleteProject)
 
 app.listen({ port: Number(env.SERVER_PORT) }).then(() => {
   console.log(`Server is running on http://localhost:${env.SERVER_PORT}`)
