511d99c05191572672f72b2e7f0669f31b160c2c
Filipe Brochier <filipe.brochier@gmail.com>
2025-12-23 18:24:30 -0300

    feat: implemented [POST] /organizations/:slug/projects

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -25,6 +25,7 @@ import { getOrganizations } from './routes/orgs/get-organizations'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
+import { createProject } from './routes/projects/create-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -78,6 +79,8 @@ app.register(getMembership)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
 app.register(transferOrganization)
+
+app.register(createProject)
 
 app.listen({ port: Number(env.SERVER_PORT) }).then(() => {
   console.log(`Server is running on http://localhost:${env.SERVER_PORT}`)
