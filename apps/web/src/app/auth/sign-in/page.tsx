32f5e2aa563b7a822e4ad2a29e7b86f5bdce687b
Filipe Brochier <filipe.brochier@gmail.com>
2026-01-07 13:43:53 -0300

    feat: created auth pages

---
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import githubIcon from '@/assets/github-icon.svg'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+export default function SignInPage() {
+  return (
+    <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="email">Email</Label>
+        <Input type="email" name="email" id="email" />
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input type="password" name="password" id="password" />
+
+        <Link
+          href="/auth/forgot-password"
+          className="text-password text-foreground ml-auto block text-xs font-medium hover:underline"
+        >
+          Forgot password?
+        </Link>
+      </div>
+
+      <Button type="submit" className="w-full">
+        Sign in with email
+      </Button>
+
+      <Button variant="link" type="button" className="w-full" size="sm" asChild>
+        <Link href="/auth/sign-up">Create new account</Link>
+      </Button>
+
+      <Separator />
+
+      <Button type="submit" variant="outline" className="w-full">
+        <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
+        Sign in with GitHub
+      </Button>
+    </form>
+  )
+}
