8ce7a5fda57969dedc1d48f27ee83e05c09a1b01
Filipe Brochier <filipe.brochier@gmail.com>
2026-01-09 15:28:27 -0300

    feat: created sign-up action into the web app

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,7 @@
 import { auth } from '@/auth/auth'
-import { Button } from '@/components/ui/button'
 
 export default async function Home() {
   const { user } = await auth()
 
   return <pre>{JSON.stringify(user, null, 2)}</pre>
-  // return <Button>Sign in</Button>
 }
